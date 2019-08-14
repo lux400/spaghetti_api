c83e566ce4905d43f32785984e0daf466bfaf4b7
Volodymyr Tsymbalenko <vtsi@ciklum.com>
2019-08-14 13:56:20 +0300

    fixed grouping graphql files

--- app/schema/index.ts
+++ app/schema/index.ts
@@ -2,8 +2,5 @@ import { importSchema } from 'graphql-import';
 import { gql } from 'apollo-server-express';
 
 const typeDefs = importSchema('./app/schema/schema.graphql');
-const resolvers = {}
-// const schema = makeExecutableSchema({ typeDefs, resolvers })
-console.log(typeDefs);
 
 export default gql(typeDefs);
